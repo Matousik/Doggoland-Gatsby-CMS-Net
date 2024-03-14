8206951df8918da27999ff8553ac15c299272d1c
Matous Vondrak <matousik@gmail.com>
2024-03-14 06:30:38 +0100

    change links

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,7 +5,7 @@ const Footer = () => {
     return (
       <footer>
         <div className="content has-text-centered autor">
-          <p>© Matouš Vondrák | Design: Šárka Löffelmanová</p>
+          <p>© <a href="https://www.linkedin.com/in/matous-vondrak/" target="_blank">Matouš Vondrák</a> | Design: <a href="https://www.linkedin.com/in/%C5%A1%C3%A1rka-l%C3%B6ffelmannov%C3%A1/" target="_blank">Šárka Löffelmanová</a></p>
         </div>
       </footer>
     );
